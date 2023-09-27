0110c3dd5b22017af3e97f6fdf79bb8c215281c6
marant <mf2018000597@virtual.upt.pe>
2023-09-26 22:51:49 -0500

    añadiendo un form

---
+++ Ejercicio2test/EjercicioDaleska.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio2test
+{
+    public partial class EjercicioDaleska : Form
+    {
+        public EjercicioDaleska()
+        {
+            InitializeComponent();
+        }
+    }
+}
