ac908a67275ac11f66a04d045f4d931911467051
Daleskadf <df2021070308@virtual.upt.pe>
2023-09-26 21:40:42 -0500

    sigo editando 2

--- ejercicio3test/Form1.Designer.cs
+++ ejercicio3test/Form1.Designer.cs
@@ -35,12 +35,17 @@
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button2
             // 
             this.button2.BackColor = System.Drawing.Color.LightSteelBlue;
-            this.button2.Location = new System.Drawing.Point(502, 112);
+            this.button2.Location = new System.Drawing.Point(517, 23);
             this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(295, 70);
@@ -75,7 +80,7 @@
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.GreenYellow;
-            this.button1.Location = new System.Drawing.Point(56, 50);
+            this.button1.Location = new System.Drawing.Point(12, 23);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(345, 113);
             this.button1.TabIndex = 5;
@@ -98,11 +103,58 @@
             this.textBox2.Size = new System.Drawing.Size(551, 26);
             this.textBox2.TabIndex = 7;
             // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.MediumPurple;
+            this.textBox3.Location = new System.Drawing.Point(117, 458);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(551, 26);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.Color.DarkGreen;
+            this.textBox4.Location = new System.Drawing.Point(117, 501);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(551, 26);
+            this.textBox4.TabIndex = 9;
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.Color.DarkGreen;
+            this.textBox5.Location = new System.Drawing.Point(117, 436);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(551, 26);
+            this.textBox5.TabIndex = 10;
+            // 
+            // textBox6
+            // 
+            this.textBox6.BackColor = System.Drawing.Color.MidnightBlue;
+            this.textBox6.Location = new System.Drawing.Point(117, 417);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(551, 26);
+            this.textBox6.TabIndex = 11;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.GreenYellow;
+            this.button3.Location = new System.Drawing.Point(534, 148);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(345, 113);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(900, 562);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
@@ -127,6 +179,11 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button3;
     }
 }
 
