090c5462dc3b35a93350ed2d4d99cb75df58475c
marant <mf2018000597@virtual.upt.pe>
2023-09-26 22:14:19 -0500

    estoy editando mi boton

--- ejercicio3test/Form1.Designer.cs
+++ ejercicio3test/Form1.Designer.cs
@@ -40,49 +40,47 @@
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.textBox6 = new System.Windows.Forms.TextBox();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button2
             // 
             this.button2.BackColor = System.Drawing.Color.LightSteelBlue;
-            this.button2.Location = new System.Drawing.Point(517, 23);
-            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button2.Location = new System.Drawing.Point(460, 18);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(295, 70);
+            this.button2.Size = new System.Drawing.Size(262, 56);
             this.button2.TabIndex = 1;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = false;
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(340, 321);
-            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dateTimePicker1.Location = new System.Drawing.Point(302, 257);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(350, 26);
+            this.dateTimePicker1.Size = new System.Drawing.Size(312, 22);
             this.dateTimePicker1.TabIndex = 2;
             // 
             // dateTimePicker2
             // 
-            this.dateTimePicker2.Location = new System.Drawing.Point(340, 370);
-            this.dateTimePicker2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dateTimePicker2.Location = new System.Drawing.Point(302, 296);
             this.dateTimePicker2.Name = "dateTimePicker2";
-            this.dateTimePicker2.Size = new System.Drawing.Size(350, 26);
+            this.dateTimePicker2.Size = new System.Drawing.Size(312, 22);
             this.dateTimePicker2.TabIndex = 3;
             // 
             // dateTimePicker3
             // 
-            this.dateTimePicker3.Location = new System.Drawing.Point(340, 268);
-            this.dateTimePicker3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dateTimePicker3.Location = new System.Drawing.Point(302, 214);
             this.dateTimePicker3.Name = "dateTimePicker3";
-            this.dateTimePicker3.Size = new System.Drawing.Size(350, 26);
+            this.dateTimePicker3.Size = new System.Drawing.Size(312, 22);
             this.dateTimePicker3.TabIndex = 4;
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.GreenYellow;
-            this.button1.Location = new System.Drawing.Point(12, 23);
+            this.button1.Location = new System.Drawing.Point(11, 18);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(345, 113);
+            this.button1.Size = new System.Drawing.Size(307, 90);
             this.button1.TabIndex = 5;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = false;
@@ -90,66 +88,83 @@
             // textBox1
             // 
             this.textBox1.BackColor = System.Drawing.Color.DarkGreen;
-            this.textBox1.Location = new System.Drawing.Point(66, 208);
+            this.textBox1.Location = new System.Drawing.Point(59, 166);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(551, 26);
+            this.textBox1.Size = new System.Drawing.Size(490, 22);
             this.textBox1.TabIndex = 6;
             // 
             // textBox2
             // 
             this.textBox2.BackColor = System.Drawing.Color.DarkSalmon;
-            this.textBox2.Location = new System.Drawing.Point(117, 480);
+            this.textBox2.Location = new System.Drawing.Point(104, 384);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(551, 26);
+            this.textBox2.Size = new System.Drawing.Size(490, 22);
             this.textBox2.TabIndex = 7;
             // 
             // textBox3
             // 
             this.textBox3.BackColor = System.Drawing.Color.MediumPurple;
-            this.textBox3.Location = new System.Drawing.Point(117, 458);
+            this.textBox3.Location = new System.Drawing.Point(104, 366);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(551, 26);
+            this.textBox3.Size = new System.Drawing.Size(490, 22);
             this.textBox3.TabIndex = 8;
             // 
             // textBox4
             // 
             this.textBox4.BackColor = System.Drawing.Color.DarkGreen;
-            this.textBox4.Location = new System.Drawing.Point(117, 501);
+            this.textBox4.Location = new System.Drawing.Point(104, 401);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(551, 26);
+            this.textBox4.Size = new System.Drawing.Size(490, 22);
             this.textBox4.TabIndex = 9;
             // 
             // textBox5
             // 
             this.textBox5.BackColor = System.Drawing.Color.DarkGreen;
-            this.textBox5.Location = new System.Drawing.Point(117, 436);
+            this.textBox5.Location = new System.Drawing.Point(104, 349);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(551, 26);
+            this.textBox5.Size = new System.Drawing.Size(490, 22);
             this.textBox5.TabIndex = 10;
             // 
             // textBox6
             // 
             this.textBox6.BackColor = System.Drawing.Color.MidnightBlue;
-            this.textBox6.Location = new System.Drawing.Point(117, 417);
+            this.textBox6.Location = new System.Drawing.Point(104, 334);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(551, 26);
+            this.textBox6.Size = new System.Drawing.Size(490, 22);
             this.textBox6.TabIndex = 11;
             // 
             // button3
             // 
             this.button3.BackColor = System.Drawing.Color.GreenYellow;
-            this.button3.Location = new System.Drawing.Point(534, 148);
+            this.button3.Location = new System.Drawing.Point(475, 118);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(345, 113);
+            this.button3.Size = new System.Drawing.Size(307, 90);
             this.button3.TabIndex = 12;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = false;
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(11, 214);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(225, 97);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "esta es mi edicion";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(900, 562);
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.textBox6);
             this.Controls.Add(this.textBox5);
@@ -162,7 +177,6 @@
             this.Controls.Add(this.dateTimePicker2);
             this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.button2);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -184,6 +198,7 @@
         private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.TextBox textBox6;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
